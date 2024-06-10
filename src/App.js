b347b06e5249c11ab6991b3c2cc4a7ce341bb9ca
Lindsey Eom <linand02@protiviti.com>
2024-06-10 09:26:00 -0400

    new files added

--- src/App.js
+++ src/App.js
@@ -16,33 +16,6 @@ let singleUser = {
   todaysDate: date.toDateString(),
 }
 
-let allMovies = [
-  {
-    movieTitle: 'Se7en',
-    movieImage: 'https://static.tvtropes.org/pmwiki/pub/images/se7en_poster.png',
-    movieRelease: '1995',
-    movieReview:'',
-  },
-  {
-    movieTitle: '12 Angry Men',
-    movieImage: 'https://m.media-amazon.com/images/M/MV5BNDhjMjE4NDItZTkyOC00NjIwLWI0MDQtYTJhZjY2YzlkMDQ0XkEyXkFqcGdeQXVyMTA0MTM5NjI2._V1_.jpg',
-    movieRelease: '1957',
-    movieReview:'',
-  },
-  {
-    movieTitle: 'Harry Potter and the Half-Blood Prince',
-    movieImage: 'https://static.wikia.nocookie.net/harrypotter/images/4/45/Harry_and_Dumbledore_HBPF_promo.jpg/',
-    movieRelease: '2009',
-    movieReview:'',
-  },
-  {
-    movieTitle: 'Jaws',
-    movieImage: 'https://s3.amazonaws.com/nightjarprod/content/uploads/sites/192/2021/05/10145225/l1yltvzILaZcx2jYvc5sEMkM7Eh.jpg',
-    movieRelease: '1975',
-    movieReview:'',
-  }
-];
-
 function App() {
   const[movies, setMovies] = useState(allMovies)
   return (
